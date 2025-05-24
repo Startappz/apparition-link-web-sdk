c58f0df8650c39e69577d16908d6c1f1dd5be36c
Deployment Script <deploy@apparition.link>
2025-05-24 23:31:20 +0400

    fix(deploy-qa.sh): Adding cloudfront integration.

--- deployment/deploy-qa.sh
+++ deployment/deploy-qa.sh
@@ -9,20 +9,27 @@ GREEN='\033[0;32m'
 NC='\033[0m'
 
 #--------------------------------------------------------------------------------------------
-# Main branch (QA) Deploy
+# Main apparition (QA) Deploy
 #--------------------------------------------------------------------------------------------
 
-  echo -en "${GREEN}QA Release...${NC}\n"
+echo -en "${GREEN}QA Release...${NC}\n"
 
-  echo -en "${GREEN}make release ...${NC}\n"
-  make release
+echo -en "${GREEN}make release ...${NC}\n"
+make release
 
-  echo -en "${GREEN}Pushing to builds ...${NC}\n"
-  aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://apparition-builds/web-sdk/apparition-latest.min.js
-  aws s3 cp --content-type="text/javascript" dist/build.js s3://apparition-builds/web-sdk/apparition.js
+echo -en "${GREEN}Pushing to builds ...${NC}\n"
+aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://apparition-builds/web-sdk/apparition-latest.min.js
+aws s3 cp --content-type="text/javascript" dist/build.js s3://apparition-builds/web-sdk/apparition.js
 
-  ./deployment/build-example-html.sh "key_live_qgNPe4TZTuZSoLjPBZdw8w" "https://stg.apparition.link/api" "https://cdn.apparition.link/apparition-staging-latest.min.js"
-  # aws s3 cp example.html s3://apparition-cdn/example-staging.html
+./deployment/build-example-html.sh "key_live_qgNPe4TZTuZSoLjPBZdw8w" "https://stg.apparition.link/api" "https://cdn.apparition.link/apparition-staging-latest.min.js"
+aws s3 cp example.html s3://apparition-cdn/example-staging.html
+
+echo -en "${GREEN}Pushing to CDN ...${NC}\n"
+aws s3 cp --content-type="text/javascript" --content-encoding="gzip" dist/build.min.js.gz s3://apparition-cdn/apparition-staging-latest.min.js --cache-control "max-age=300"
+
+echo -en "Invalidating cloudfront distribution for staging ...\n"
+aws configure set preview.cloudfront true
+aws cloudfront create-invalidation --distribution-id EPLQ6YI8ZXNYS --paths /apparition-staging-latest.min.js /example-staging.html
 
 
 # Exit prompts
